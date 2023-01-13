b47a62e21c0eb766ca28ba233379cce1ad7d4def
= <=>
2023-01-13 23:21:01 +0530

    basic setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// it("render wihtout crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<App />, div);
+
+//   expect(div.innerHTML).toContain("Hi there");
+
+//   ReactDOM.unmountComponentAtNode(div);
