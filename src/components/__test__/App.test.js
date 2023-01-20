14b4ce467c7365cfda1b3def967546851990b171
= <=>
2023-01-20 23:02:21 +0530

    testing comment list

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -5,9 +5,16 @@ import "../../setupTest";
 
 import App from "../App";
 import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it("Shows a comment box", () => {
   const wrapped = shallow(<App />);
 
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("Show a comment list", () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
