aa0b012dcbbeeb197da12cb56fd568eca4c262df
= <=>
2023-01-26 22:56:16 +0530

    error fix

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
 
+import "setupTest";
+
 it("Shows a comment box", () => {
   const div = document.createElement("div");
 
--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { mount } from "enzyme";
 
-import CommentBox from "components/CommentBox";
+import CommentBox from "../CommentBox";
 import "setupTest";
 
 it("Has a text area and a button", () => {
   const wrapped = mount(<CommentBox />);
-  console.log(wrapped.find("textarea").length);
-  console.log(wrapped.find("button").length);
+  //   console.log(wrapped.find("textarea").length);
+  //   console.log(wrapped.find("button").length);
 
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
