ed6a2088d959ac30ca37f8191a745764ae466834
= <=>
2023-01-26 22:46:30 +0530

    test case added for comment box

---
+++ src/components/__test__/CommentBox.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import CommentBox from "components/CommentBox";
+import "setupTest";
+
+it("Has a text area and a button", () => {
+  const wrapped = mount(<CommentBox />);
+  console.log(wrapped.find("textarea").length);
+  console.log(wrapped.find("button").length);
+
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
