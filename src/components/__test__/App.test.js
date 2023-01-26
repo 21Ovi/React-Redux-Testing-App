f96fa07cb1d2ba93034b3a20d86be69941714310
= <=>
2023-01-26 23:03:33 +0530

    error fix 2

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import "../../setupTest";
+import "setupTest";
 
 import App from "components/App";
 import CommentBox from "components/CommentBox";
--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -2,13 +2,15 @@ import React from "react";
 import { mount } from "enzyme";
 
 import CommentBox from "../CommentBox";
-import "../../setupTest";
+import "setupTest";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
 
 it("Has a text area and a button", () => {
-  const wrapped = mount(<CommentBox />);
-  //   console.log(wrapped.find("textarea").length);
-  //   console.log(wrapped.find("button").length);
-
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
