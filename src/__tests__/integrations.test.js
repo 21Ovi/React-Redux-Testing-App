9b60098d8b5d3abfa65ebd6ac55c1104a3ba3844
= <=>
2023-02-27 00:40:17 +0530

    integration test skeleton added

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { mount } from "enzyme";
+import Root from "Root";
+import App from "components/App";
+
+it("can fetch a list of comments and display them", () => {
+  //  Attempt to render the *entire* app
+  //  find the 'fetch comments' byutton and click it
+  // Expect ti find a list of comments!
+});
