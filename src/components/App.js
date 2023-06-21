81be50289f852bbdde0314c5fd9e44aa1aade591
= <=>
2023-06-21 22:44:36 +0530

    App converted into class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { Component } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" exact element={<CommentList />} />
-      <Route path="/post" element={<CommentBox />} />
-    </Routes>
-  );
-};
+class App extends Component {
+  renderButton() {}
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <Routes>
+        <Route path="/" exact element={<CommentList />} />
+        <Route path="/post" element={<CommentBox />} />
+      </Routes>
+    );
+  }
+}
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import commentReducers from "reducers/comments";
+import authReucer from "reducers/auth";
 
 export default combineReducers({
   comments: commentReducers,
+  auth: authReucer,
 });
