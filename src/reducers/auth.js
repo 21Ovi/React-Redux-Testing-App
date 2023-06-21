ce48b28992ab095bf340dfe83f3b16f4295e7ed2
= <=>
2023-06-21 22:31:46 +0530

    auth reducer added

---
+++ src/reducers/auth.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { CHANGE_AUTH } from "actions/types";
+
+export default (state = false, action) => {
+  switch (action.type) {
+    case CHANGE_AUTH:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
